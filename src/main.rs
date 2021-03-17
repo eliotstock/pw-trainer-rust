80e5e94adca7259e5a8beba8bd4cda53ad63bc90
Eliot Stock <1@eliotstock.com>
2021-03-17 22:29:50 +1300

    Operation matches.

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,44 @@ use bcrypt::{DEFAULT_COST, hash, verify};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // println!("{:?}", args);
+
+    if args.len() != 3 {
+        usage(&args[0]);
+        std::process::exit(1);
+    }
 
     let op = &args[1];
     let pw = &args[2];
 
     match op.as_str() {
         "set" => {
-            let hashed = hash("foo", DEFAULT_COST).unwrap();
+            let hash = hash(pw, DEFAULT_COST).unwrap();
 
             println!("TODO: Put the hash in storage.");
         },
         "check" => {
             // TODO: Get the hash of the pw from storage.
-            let hashed = hash("foo", DEFAULT_COST).unwrap();
+            let hash = "sdfklskdjlc";
 
-            let valid = verify(pw, &hashed).unwrap();
-            println!("Valid: {}", valid);
+            let valid = verify(pw, &hash).unwrap();
+
+            if valid {
+                println!("Password correct");
+            }
+            else {
+                println!("Password INCORRECT");
+            }
+        },
+        "--help" => {
+            usage(&args[0]);
+            std::process::exit(1);
         },
         _ => {
             println!("Not an operation: {}.", op);
         }
     }
+}
+
+fn usage(executable: &str) {
+    println!("Usage: {} [set|check] password", executable);
 }
